98c9ca913c1a771f731e8b63f2ea78ea50f3f717
sunilthitme <thitamesunil808@gmail.com>
2021-12-09 18:02:13 +0530

    Ensure player position exact 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,37 +1,49 @@
 package com.snakeladder;
 
 public class SnakeLadder {
+		public static void main(String[] args) {
+			int position = 0;
+			System.out.println("Welcome into Snake and Ladder program !! ");
+			System.out.println("position is " + position);
+			int diceValue = (int) (Math.random() * 10) % 6 + 1;
+			Utility utility = new Utility();
+			utility.playOption(position, diceValue);
 
-	public static void main(String[] args) {
-		System.out.println("Welcome into Snake and Ladder game");
-		int position=0;
-		System.out.println("Your Starting position is::" + position);
-		
-		int Die=(int)Math.floor(Math.random()*10)%6+1;
-		System.out.println("Your Die is :" +Die);
-		
-		for(position=0; position<=100; position++ ) {
-			int playerCheck=(int)Math.floor(Math.random()*10)%3+1;
-		switch (playerCheck) {
-		case 1:
-			System.out.println("No Play You are in the same position");
-			break;
-		case 2:
-			position=position+Die;
-			System.out.println("your position after ladder is ::" + position);
-			break;
-			
-		case 3:
-			position=position-Die;
-			
-			if (position<0) {
-					  position = 0;
-				  }
-					 System.out.println( "Your Position after the snake is::" + position );
-				   break;
-			
 		}
-		
 	}
+
+	class Utility {
+
+		public void playOption(int position, int diceValue) {
+
+			while (position <= 99) {
+
+				int optionValue = (int) (Math.random() * 10) % 3 + 1;
+
+				switch (optionValue) {
+				case 1:
+					System.out.println("No Play....You are in the same position");
+					break;
+
+				case 2:
+					position = (position + diceValue);
+					if (position > 100) {
+						position = (position - diceValue);
+						continue;
+					}
+					System.out.println("your position after ladder is :" + position);
+					break;
+
+				case 3:
+					position = (position - diceValue);
+
+					if (position < 0) {
+						position = 0;
+					}
+					System.out.println("Your Position after snake is" + position);
+					break;
+				}
+			}
+		}
+
 	}
-}
