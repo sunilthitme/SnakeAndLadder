f879e14b7f0165550ce5b2cf98bfcb05360514e8
sunilthitme <thitamesunil808@gmail.com>
2021-12-09 16:41:05 +0530

    Use Random number to check options

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,8 +7,30 @@ public class SnakeLadder {
 		int position=0;
 		System.out.println("Your Starting position is::" + position);
 		
-		int Dice=(int)Math.floor(Math.random()*10)%6+1;
-		System.out.println("Your Dice is :" +Dice);
+		int Die=(int)Math.floor(Math.random()*10)%6+1;
+		System.out.println("Your Die is :" +Die);
+		int playerCheck=(int)Math.floor(Math.random()*10)%3+1;
+		
+		switch (playerCheck) {
+		case 1:
+			System.out.println("No Play You are in the same position");
+			break;
+		case 2:
+			position=position+Die;
+			System.out.println("your position after ladder is ::" + position);
+			break;
+			
+		case 3:
+			position=position-Die;
+			System.out.println("your position get back by::" + position);
+			
+			if (position<0) {
+					  position = 0;
+				  }
+					 System.out.println( "Your current Position is::" + position );
+				   break;
+			
+		}
 		
 	}
 
